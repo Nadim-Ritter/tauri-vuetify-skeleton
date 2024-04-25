de6a355f45ec5e0c886934771129542ba5876d24
Nadim Ritter <nadim.ritter@let.ethz.ch>
2024-04-25 16:57:15 +0200

    skeleton

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container fluid>
+  
+      <v-row>
+        <v-col>
+            <v-sheet 
+                elevation="4"
+                class="rounded-lg"
+                title="Hallo">
+  
+                test
+                
+            </v-sheet>
+            <v-btn icon="mdi-account"></v-btn>
+        </v-col>
+      </v-row>
+  
+    </v-container>
+  </template>
+  
+  
+  <script setup lang="ts">
+  
+  
+  </script>
+  
+  <style scoped>
+  </style>
+  
